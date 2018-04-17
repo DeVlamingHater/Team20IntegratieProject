6c792640a44ac935b4568ceff808374ed62cd794
Sam.Claessen <sam.claessen@student.kdg.be>
2018-04-17 09:36:51 +0200

    DB overige klassen

---
+++ Domain/Dashboards/Grafiek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dashboards
+{
+    class Grafiek : Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public List<DataConfig> Dataconfig{ get; set; }
+    }
+}
---
+++ Domain/Dashboards/Zone.cs
@@ -0,0 +1,19 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dashboards
+{
+    class Zone
+    {
+        public int Id{ get; set; }
+
+        public Dashboard Dashboard { get; set; }
+
+        public string Naam { get; set; }
+
+
+    }
+}
