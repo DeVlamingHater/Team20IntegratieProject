75531ac4e64fe6e9987d9b89fdf7b0a88c504712
DaanJacobs11 <daanjacobs@skynet.be>
2018-04-20 14:54:47 +0200

    zones verwijderen

---
+++ PolitiekeBarometer_MVC/Scripts/EigenScripts.js
@@ -0,0 +1,10 @@
+﻿<script>
+    function button_click(objTextBox,objBtnID)
+    {
+        if(window.event.keyCode==13)
+        {
+        document.getElementById(objBtnID).focus();
+    document.getElementById(objBtnID).click();
+        }
+    }
+</script>
