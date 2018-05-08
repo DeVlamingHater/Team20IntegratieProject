a1ec9a17b58187b75c62bec0df73c93112258c46
potathomas <thomas.somers@student.kdg.be>
2018-05-08 08:43:13 +0200

    Werken android api

--- PolitiekeBarometer_MVC/Controllers/AndroidApiController.cs
+++ PolitiekeBarometer_MVC/Controllers/AndroidApiController.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
 
 namespace PolitiekeBarometer_MVC.Controllers
 {
     public class AndroidApiController : ApiController
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "User1", "User2" };
-        }
+        //[HttpGet]
+        //[Authorize(Roles ="Admin,SuperAdmin",Claims="Platform|1")]
+        //public IEnumerable<string> Get()
+        //{
+        //    var user = this.User as System.Security.Claims.ClaimsPrincipal;
+        //    user.Claims.SingleOrDefault();
+        //    //var newClaim = new Claim("Platform", "1");
+        //    (this.User.Identity as ClaimsIdentity).HasClaim(c => c.Type == "Platform" && c.Value == "1");
+        //    identity.
+        //    return new string[] { "User1", "User2" };
+        //}
+
+
+        
     }
 }
