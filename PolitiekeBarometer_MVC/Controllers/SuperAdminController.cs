1877d7eda46e14536433ec5a8b6e7712a28dda5d
Sam.Claessen <sam.claessen@student.kdg.be>
2018-05-09 12:13:34 +0200

    Admin createGebruiker fix

--- PolitiekeBarometer_MVC/Controllers/SuperAdminController.cs
+++ PolitiekeBarometer_MVC/Controllers/SuperAdminController.cs
@@ -1,6 +1,7 @@
 ﻿using BL.Interfaces;
 using BL.Managers;
 using DAL.EF;
+using Domain.Platformen;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using PolitiekeBarometer_MVC.Models;
@@ -31,24 +32,30 @@ namespace PolitiekeBarometer_MVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult CreateUser(FormCollection form)
         {
+            var user = new ApplicationUser();
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
             string naam = form["txtNaam"];
             string userName = form["txtEmail"];
             string email = form["txtEmail"];
             string password = form["txtPassword"];
-
-            var user = new ApplicationUser();
+            Gebruiker gebruiker = new Gebruiker()
+            {
+                Email = user.Email,
+                Naam = user.Name,
+                GebruikerId = user.Id
+            };
             user.Name = naam;
             user.UserName = userName;
             user.Email = email;
             string pwd = password;
 
+            user.Gebruiker = gebruiker;
             IPlatformManager platformManager = new PlatformManager();
-            
-            var newuser = userManager.Create(user, pwd);
-           ;
 
-            platformManager.createGebruiker(userManager.Users.First(u => u.Email == user.Email).Id, user.Name, user.Email);
+            var newuser = userManager.Create(user, pwd);
+            string id = user.Id;
+
+            platformManager.createGebruiker(gebruiker.GebruikerId, gebruiker.Naam, gebruiker.Email);
             return View("Index");
         }
         public ActionResult CreateRole()
