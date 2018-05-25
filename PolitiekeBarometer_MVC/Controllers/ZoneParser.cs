19e230b16fbe389611e50689bc049f3639e3a886
potathomas <thomas.somers@student.kdg.be>
2018-05-25 21:21:35 +0200

    niet acceptere

---
+++ PolitiekeBarometer_MVC/Controllers/ZoneParser.cs
@@ -0,0 +1,144 @@
+﻿using BL.Interfaces;
+using BL.Managers;
+using Domain;
+using Domain.Dashboards;
+using PolitiekeBarometer_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PolitiekeBarometer_MVC.Controllers
+{
+    public class ZoneParser
+    {
+        public static List<ZoneViewModel> ParseZones(List<Zone> zones, UnitOfWorkManager uowmgr)
+        {
+            int itemid = 1;
+            int zoneId = 1;
+            List<ZoneViewModel> zonesViewModel = new List<ZoneViewModel>();
+
+            IDashboardManager dashboardManager = new DashboardManager(uowmgr);
+
+            foreach (Zone zone in zones)
+            {
+                ZoneViewModel zoneViewModel = new ZoneViewModel()
+                {
+                    naam = zone.Naam,
+                    zoneId = zone.Id,
+                    items = new List<ItemViewModel>(),
+                    plaatsId = zoneId
+                };
+                zoneId++;
+                if (zone.Items != null)
+                {
+                    foreach (Item item in zone.Items)
+                    {
+                        if (item.GetType() == typeof(Grafiek))
+                        {
+                            Grafiek grafiek = dashboardManager.getGrafiek(item.Id);
+                            GrafiekViewModel itemViewModel = new GrafiekViewModel()
+                            {
+                                tittel = item.Tittel,
+                                DataType = grafiek.DataType,
+                                GrafiekType = grafiek.GrafiekType,
+                                datasets = new Dictionary<string, Dictionary<string, double>>(),
+                                id = itemid,
+                                itemId = item.Id
+                            };
+                            itemid++;
+                            dashboardManager.getGraphData(grafiek);
+                            zone.Items.Add(grafiek);
+                        }
+                    }
+                }
+                zonesViewModel.Add(zoneViewModel);
+
+
+            }
+            #region testData
+            ZoneViewModel testZone = new ZoneViewModel()
+            {
+                naam = "testZone",
+                items = new List<ItemViewModel>(),
+                plaatsId = zoneId
+            };
+            zoneId++;
+            ZoneViewModel legeZone = new ZoneViewModel()
+            {
+                naam = "legeZone",
+                items = new List<ItemViewModel>(),
+                plaatsId = zoneId
+            };
+            zoneId++;
+            #region testBar
+            Dictionary<string, Dictionary<string, double>> datasets = new Dictionary<string, Dictionary<string, double>>();
+            Dictionary<string, double> dataset = new Dictionary<string, double>();
+            dataset.Add("Bart", 20.0);
+            dataset.Add("Imade", 80.0);
+            datasets.Add("", dataset);
+
+            GrafiekViewModel testGrafiek = new GrafiekViewModel()
+            {
+                tittel = "testGrafiek",
+                datasets = datasets,
+                GrafiekType = GrafiekType.BAR,
+                DataType = DataType.TOTAAL,
+                id = itemid
+            };
+            itemid++;
+            testZone.items.Add(testGrafiek);
+            #endregion
+
+            #region testPie
+            Dictionary<string, Dictionary<string, double>> datasetsPie = new Dictionary<string, Dictionary<string, double>>();
+            Dictionary<string, double> datasetPie = new Dictionary<string, double>();
+            datasetPie.Add("Bart", 20.0);
+            datasetPie.Add("Imade", 80.0);
+            datasetsPie.Add("", dataset);
+
+            GrafiekViewModel testGrafiekPie = new GrafiekViewModel()
+            {
+                tittel = "testGrafiek",
+                datasets = datasetsPie,
+                GrafiekType = GrafiekType.PIE,
+                DataType = DataType.TOTAAL,
+                id = itemid
+            };
+            itemid++;
+            testZone.items.Add(testGrafiekPie);
+            #endregion
+            #region testLine
+
+            Dictionary<string, Dictionary<string, double>> datasetsLine = new Dictionary<string, Dictionary<string, double>>();
+            Dictionary<string, double> datasetLine1 = new Dictionary<string, double>();
+            datasetLine1.Add(DateTime.Now.AddDays(-4).ToShortDateString(), 20.0);
+            datasetLine1.Add(DateTime.Now.AddDays(-3).ToShortDateString(), 30.0);
+
+            Dictionary<string, double> datasetLine2 = new Dictionary<string, double>();
+            datasetLine2.Add(DateTime.Now.AddDays(-4).ToShortDateString(), 50.0);
+            datasetLine2.Add(DateTime.Now.AddDays(-3).ToShortDateString(), 40.0);
+
+            datasetsLine.Add("Lijn 1", datasetLine1);
+            datasetsLine.Add("Lijn 2", datasetLine2);
+
+            GrafiekViewModel testLine = new GrafiekViewModel()
+            {
+                tittel = "testLine",
+                datasets = datasetsLine,
+                GrafiekType = GrafiekType.LINE,
+                DataType = DataType.TOTAAL,
+                id = itemid
+            };
+            itemid++;
+            testZone.items.Add(testLine);
+            #endregion
+            zonesViewModel.Add(testZone);
+            zonesViewModel.Add(legeZone);
+            #endregion
+
+
+            return zonesViewModel;
+        }
+    }
+}
---
+++ PolitiekeBarometer_MVC/Scripts/Grafiek/GrafiekFuncties.js
@@ -0,0 +1,212 @@
+﻿var menuOpen = false;
+function menuFunction(el) {
+    //alert("menu")
+    var test = el.parentElement;
+    //var test = document.getElementById('@ViewBag.grafiekButtons');
+    var menuIcons = test.children[0];
+    var btnMenu = test.children[1];
+    var iconMenu = btnMenu.children[0];
+    if (menuOpen === false) {
+        iconMenu.style.transform = "rotate(90deg)";
+        btnMenu.setAttribute("class", "btn btn-secondary");
+        menuIcons.style.visibility = 'visible';
+        menuOpen = true;
+    } else {
+        iconMenu.style.transform = "none";
+        btnMenu.setAttribute("class", "btn btn-default");
+        menuIcons.style.visibility = "hidden";
+        menuOpen = false;
+    };
+}
+// Verplaatsen naar dashboard
+function iconDashboardFunction(el) {
+    //alert("iconDash " + el);
+    var modal = el.parentElement.parentElement;
+    //alert(modal.getAttribute("id"));
+    var f = {};
+    f.url = '@Url.Action("getZonesJson", "Home")';
+    f.type = "POST";
+    f.dataType = "json";
+    f.contentType = "application/json";
+    f.success = function (response) {
+        alert("succes json");
+        setZoneLijstFunction(response, modal);
+    };
+    //f.error = function (response) {
+    //    alert("failed");
+    //};
+    $.ajax(f);
+}
+function setZoneLijstFunction(response, modal) {
+    //alert("zones " + modal);
+    var grafiek = modal.id;
+    var grafiekindex = '';
+    for (i = 0; i < grafiek.length; i++) {
+        if (isNaN(grafiek[i]) === false) {
+            grafiekindex = grafiekindex + grafiek[i];
+        }
+    };
+    //alert("grafindex = " + grafiekindex);
+    //alert("setLijst");
+    var select = document.getElementById("zones");
+    select.innerHTML = "";
+    for (i = 0; i < response.length; i++) {
+        alert("setlijst loop " + response);
+        var option = document.createElement("option");
+        var a = document.createElement("a");
+        var link = "filterElement(\"" + response[i] + "\")";
+        option.innerHTML = response[i];
+        option.setAttribute("value", response[i]);
+        select.appendChild(option);
+    };
+}
+function saveGrafiek(el) {
+    var parent = el.parentElement.parentElement.parentElement.parentElement;
+    var grafiek = parent.id;
+    for (i = 0; i < grafiek.length; i++) {
+        if (isNaN(grafiek[i]) === false) {
+            grafiekindex = grafiekindex + grafiek[i];
+        }
+    };
+    var select = parent.children[0].children[0].children[1].children[1];
+    alert(select.id);
+    var zone = select.options[select.selectedIndex].value;
+    var f = {};
+    f.url = '@Url.Action("dashboardGrafiek", "Home")';
+    f.type = "POST";
+    f.dataType = "json";
+    f.data = JSON.stringify({ zone: zone, grafiekIndex: grafiekindex });
+    f.contentType = "application/json";
+    f.success = function (response) {
+        alert("grafiek opgeslagen");
+    };
+    f.error = function (response) {
+        alert("failed");
+    };
+    $.ajax(f);
+}
+
+// allert grafiek
+function alertSelect(el) {
+    var option = el.options[el.selectedIndex].value
+    var radioform = el.parentElement.children[3];
+    var posRadio = radioform.children[2].children[0];
+    var negRadio = radioform.children[3].children[0];
+    if (option === 'trend') {
+
+        posRadio.style.visibility = "hidden";
+        negRadio.style.visibility = "hidden";
+    } else {
+
+        posRadio.style.visibility = "visible";
+        negRadio.style.visibility = "visible";
+    }
+}
+function saveAlert(el) {
+    var parent = el.parentElement.parentElement.parentElement.parentElement;
+
+    var grafiek = parent.id;
+    for (i = 0; i < grafiek.length; i++) {
+        if (isNaN(grafiek[i]) === false) {
+            grafiekindex = grafiekindex + grafiek[i];
+        }
+    };
+    var modalbody = parent.children[0].children[0].children[1];
+    var alertselect = modalbody.children[1];
+    var radioform = modalbody.children[3];
+    var meldform = modalbody.children[6];
+    var stijgtRadio = radioform.children[0].children[0].children[0];
+    var stijgtPercent = radioform.children[0].children[0].children[1];
+    var daaltRadio = radioform.children[1].children[0].children[0];
+    var daaltPercent = radioform.children[1].children[0].children[1];
+    var posRadio = radioform.children[2].children[0].children[0];
+    var posPercent = radioform.children[2].children[0].children[1];
+    var negRadio = radioform.children[3].children[0].children[0];
+    var negPercent = radioform.children[3].children[0].children[1];
+    var browser = meldform.children[0].children[0].children[0].checked;
+    var mail = meldform.children[1].children[0].children[0].checked;
+    var app = meldform.children[2].children[0].children[0].checked;
+
+
+    var option = alertselect.options[alertselect.selectedIndex].value;
+    var selectedRadio = "";
+    var percentage = 0;
+
+    if (stijgtRadio.checked === true) {
+        percentage = stijgtPercent.value;
+        selectedRadio = "stijgt";
+    } else if (daaltRadio.checked === true) {
+        percentage = daaltPercent.value;
+        selectedRadio = "daalt";
+    } else if (posRadio.checked === true) {
+        percentage = posPercent.value;
+        selectedRadio = "pos";
+    } else if (negRadio.checked === true) {
+        percentage = negPercent.value;
+        selectedRadio = "neg";
+    } else {
+        alert("foutje");
+    };
+    //tot hier in orde
+    var f = {};
+    f.url = '@Url.Action("addAlert", "Home")';
+    f.type = "POST";
+    f.dataType = "json";
+    f.data = JSON.stringify({ id: grafiekindex, percentage: percentage, radio: selectedRadio, soort: option, browser: browser, mail: mail, app: app });
+    f.contentType = "application/json";
+    f.success = function (response) {
+        alert("alert opgeslagen");
+    };
+    f.error = function (response) {
+        alert("failed");
+    };
+    $.ajax(f);
+}
+
+//verwijder grafiek
+function deleteGrafiek(el) {
+    var parent = el.parentElement.parentElement.parentElement.parentElement;
+    var grafiek = parent.id;
+    for (i = 0; i < grafiek.length; i++) {
+        if (isNaN(grafiek[i]) === false) {
+            grafiekindex = grafiekindex + grafiek[i];
+        }
+    };
+    var f = {};
+    f.url = '@Url.Action("deleteGrafiek", "Home")';
+    f.type = "POST";
+    f.dataType = "json";
+    f.data = JSON.stringify({ grafiekIndex: grafiekindex });
+    f.contentType = "application/json";
+    f.success = function (response) {
+        alert("grafiek verwijderd");
+    };
+    f.error = function (response) {
+        alert("failed");
+    };
+    $.ajax(f);
+}
+
+//edit grafiek
+function editGrafiek(el) {
+    var parent = el.parentElement.parentElement.parentElement.parentElement;
+    var grafiek = parent.id;
+    for (i = 0; i < grafiek.length; i++) {
+        if (isNaN(grafiek[i]) === false) {
+            grafiekindex = grafiekindex + grafiek[i];
+        }
+    };
+    var f = {};
+    f.url = '@Url.Action("editGrafiek", "Home")';
+    f.type = "POST";
+    f.dataType = "json";
+    f.data = JSON.stringify({ grafiekIndex: grafiekindex });
+    f.contentType = "application/json";
+    f.success = function (response) {
+        alert("grafiek gewijzigd");
+    };
+    f.error = function (response) {
+        alert("failed");
+    };
+    $.ajax(f);
+}
