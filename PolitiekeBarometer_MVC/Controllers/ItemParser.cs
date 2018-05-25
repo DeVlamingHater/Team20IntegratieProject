0bf0e08fbd82bdf9077b36fd71c6e92642cc5c9d
Sam.Claessen <sam.claessen@student.kdg.be>
2018-05-25 23:16:32 +0200

    Last fix before upload

--- PolitiekeBarometer_MVC/Controllers/ItemParser.cs
+++ PolitiekeBarometer_MVC/Controllers/ItemParser.cs
@@ -10,25 +10,6 @@ namespace PolitiekeBarometer_MVC.Controllers
 {
     public class ItemParser
     {
-        public static ItemViewModel ParseItem(Item item, UnitOfWorkManager uowmgr )
-        {
-            if (item.GetType() == typeof(Grafiek))
-            {
-                Grafiek grafiek = dashboardManager.getGrafiek(item.Id);
-                GrafiekViewModel itemViewModel = new GrafiekViewModel()
-                {
-                    tittel = item.Tittel,
-                    DataType = grafiek.DataType,
-                    GrafiekType = grafiek.GrafiekType,
-                    datasets = new Dictionary<string, Dictionary<string, double>>(),
-                    id = itemid,
-                    itemId = item.Id
-                };
-                itemid++;
-                Dictionary<string, Dictionary<string, double>> graphData = dashboardManager.getGraphData(grafiek);
-                itemViewModel.datasets = (graphData);
-                zoneViewModel.items.Add(itemViewModel);
-            }
-        }
+        
     }
 }
